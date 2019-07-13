0455c4877711b5866f5b6ea3b408a0670a918d65
ag8 <andrew2000g@gmail.com>
2019-07-13 17:00:25 -0400

    fixed card implementation

--- card.py
+++ card.py
@@ -1,16 +1,10 @@
 class card():
-    
-
-    def __init__(self,value,suit):
+    def __init__(self, value, suit):
         self.value = value
         self.suit = suit
-        
-        
-    def get_value():
-        return self.value
-    
-    def get_suit():
-        return self.suit
-        
 
-    
+    def get_value(self):
+        return self.value
+
+    def get_suit(self):
+        return self.suit
