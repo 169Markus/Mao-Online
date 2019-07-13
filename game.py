e63387b103d9f31fdb4b0a011fe8bac3c20a4f73
ag8 <andrew2000g@gmail.com>
2019-07-13 19:29:58 -0400

    turns work now :')

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 from deck import Deck
+from player import Player
+from typing import List
 import random
 
 
 class Game(object):
-    def __init__(self, players):
+    def __init__(self, players: List[Player]):
         self.over = False
         self.rule_book = None
         self.draw_deck = Deck(prefilled=True)
@@ -12,7 +14,7 @@ class Game(object):
         self.num_players = len(players)
         self.history = []
         self.play_order = []
-        self.whose_turn = random.randint(self.num_players)
+        self.whose_turn = random.randrange(self.num_players)
         self.next_turn = lambda x: (x + 1) % len(players)  # can be overridden by rules
 
         self.deal()
@@ -22,10 +24,8 @@ class Game(object):
             player.hand = self.draw_deck.draw(5)
 
     def turn(self):
+        print("It's %s's turn!" % self.players[self.whose_turn].username)
         # possible mandatory actions first
-
-
-        #
 
 
         self.whose_turn = self.next_turn(self.whose_turn)
