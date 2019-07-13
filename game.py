f85676343940a9d7e4de4d25aeb82f0af4a07409
ag8 <andrew2000g@gmail.com>
2019-07-13 18:58:57 -0400

    implemented turn taking

--- game.py
+++ game.py
@@ -1,23 +1,26 @@
 from deck import Deck
-from player import Player
+import random
 
 
 class Game(object):
     def __init__(self, players):
-        self.state = 'Ongoing'
+        self.over = False
         self.rule_book = None
         self.draw_deck = Deck(prefilled=True)
         self.discard_deck = Deck()
         self.players = players
+        self.num_players = len(players)
         self.history = []
         self.play_order = []
-        self.turn = []
+        self.whose_turn = random.randint(self.num_players)
+        self.next_turn = lambda x: (x + 1) % len(players)  # can be overridden by rules
 
-        self.deal(self.players)
+        self.deal()
 
-
-
-
-    def deal(self, players):
-        for player in players:
+    def deal(self):
+        for player in self.players:
             player.hand = self.draw_deck.draw(5)
+
+    def turn(self):
+        pass
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from card import Card
 from deck import Deck
+from game import Game
 from player import Player
 
-
-
-
 players = [Player("andrew"), Player("derik"), Player("markus"), Player("hanna")]
+
+game = Game(players)
+
+while not game.over:
+    game.turn()
